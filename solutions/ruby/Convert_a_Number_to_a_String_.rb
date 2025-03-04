902d99a2ac6160f8e1aed790aabeba547b8858aa
Bimai6 <bimaivfx@gmail.com>
2025-03-04 23:08:52 +0000

    Auto: New solutions from Codewars

---
+++ solutions/ruby/Convert_a_Number_to_a_String_.rb
@@ -0,0 +1,4 @@
+Kata: Convert a Number to a String!
+Language: ruby
+Completed At: 2025-03-04 20:58:16
+ https://www.codewars.com/kata/convert-a-number-to-a-string
---
+++ solutions/ruby/Opposite_number.rb
@@ -0,0 +1,4 @@
+Kata: Opposite number
+Language: ruby
+Completed At: 2025-03-04 21:16:20
+ https://www.codewars.com/kata/opposite-number
