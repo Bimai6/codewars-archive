76e78d5de20419b0efec417e0cd230ed6fd12fba
Bimai6 <bimaivfx@gmail.com>
2025-03-04 23:48:48 +0100

    Manually added solutions

--- solutions/ruby/Convert_a_Number_to_a_String_.rb
+++ solutions/ruby/Convert_a_Number_to_a_String_.rb
@@ -2,3 +2,20 @@ Kata: Convert a Number to a String!
 URL: https://www.codewars.com/kata/convert-a-number-to-a-string
 Language: ruby
 Completed At: 2025-03-04
+
+=begin
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+=end
+
+=begin
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+=end
+
+def number_to_string(num)
+    num.to_s
+end
--- solutions/ruby/How_good_are_you_really_.rb
+++ solutions/ruby/How_good_are_you_really_.rb
@@ -2,3 +2,21 @@ Kata: How good are you really?
 URL: https://www.codewars.com/kata/how-good-are-you-really
 Language: ruby
 Completed At: 2025-03-04
+
+=begin
+There was a test in your class and you passed it. Congratulations!
+
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return true if you're better, else false!
+
+Note:
+Your points are not included in the array of your class's points. Do not forget them when calculating the average score!
+=end
+
+
+def better_than_average(arr, points)
+    points > arr.inject{ |sum, el | sum + el }.to_f / arr.size
+end
