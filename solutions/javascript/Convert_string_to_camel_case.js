7c87eecfcda2835399e1aed48b3141f98053a749
Bimai6 <bimaivfx@gmail.com>
2025-03-10 14:19:03 +0000

    Auto: New solutions from Codewars

---
+++ solutions/javascript/Convert_string_to_camel_case.js
@@ -0,0 +1,4 @@
+Kata: Convert string to camel case
+Language: javascript
+Completed At: 2025-03-10 09:12:37
+https://www.codewars.com/kata/convert-string-to-camel-case
---
+++ solutions/ruby/Find_the_smallest_integer_in_the_array.rb
@@ -0,0 +1,4 @@
+Kata: Find the smallest integer in the array
+Language: ruby
+Completed At: 2025-03-10 08:00:11
+https://www.codewars.com/kata/find-the-smallest-integer-in-the-array
