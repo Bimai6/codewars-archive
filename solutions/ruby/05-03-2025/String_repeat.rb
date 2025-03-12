3f4e20b02a7287bf8304e4def729d6f1f93beb35
Bimai6 <bimaivfx@gmail.com>
2025-03-12 21:47:10 +0000

    Auto: New solutions from Codewars

---
+++ solutions/ruby/05-03-2025/String_repeat.rb
@@ -0,0 +1,4 @@
+Kata: String repeat
+Language: ruby
+Completed At: 2025-03-05 08:12:29 UTC
+https://www.codewars.com/kata/string-repeat
---
+++ solutions/ruby/08-03-2025/You_Can_t_Code_Under_Pressure__1.rb
@@ -0,0 +1,4 @@
+Kata: You Can't Code Under Pressure #1
+Language: ruby
+Completed At: 2025-03-08 21:19:00 UTC
+https://www.codewars.com/kata/you-cant-code-under-pressure-1
