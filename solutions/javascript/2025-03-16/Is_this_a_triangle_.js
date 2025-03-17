1f4b0104e0332514a8ffce2f88358001ee89f02f
Bimai6 <bimaivfx@gmail.com>
2025-03-17 14:19:23 +0000

    Auto: New solutions from Codewars

---
+++ solutions/javascript/2025-03-16/Is_this_a_triangle_.js
@@ -0,0 +1,4 @@
+Kata: Is this a triangle?
+Language: javascript
+Completed At: 2025-03-16 16:15:43 UTC
+https://www.codewars.com/kata/is-this-a-triangle
---
+++ solutions/javascript/2025-03-16/Sort_the_odd.js
@@ -0,0 +1,4 @@
+Kata: Sort the odd
+Language: javascript
+Completed At: 2025-03-16 16:51:09 UTC
+https://www.codewars.com/kata/sort-the-odd
