d4d23699cde65081bb18b444ef83b39c548259b6
Bimai6 <bimaivfx@gmail.com>
2025-04-07 14:19:32 +0000

    Auto: New solutions from Codewars

---
+++ solutions/javascript/2025-04-07/Convert_boolean_values_to_strings__Yes__or__No__.js
@@ -0,0 +1,4 @@
+Kata: Convert boolean values to strings 'Yes' or 'No'.
+Language: javascript
+Completed At: 2025-04-07 08:53:54 UTC
+https://www.codewars.com/kata/convert-boolean-values-to-strings-yes-or-no
