17ff726eda4c3cbf67e7ef3cbc496c5f97eab09d
Bimai6 <bimaivfx@gmail.com>
2025-07-16 14:20:35 +0000

    Auto: New solutions from Codewars

---
+++ solutions/python/2025-07-15/Delete_occurrences_of_an_element_if_it_occurs_more_than_n_times.py
@@ -0,0 +1,4 @@
+Kata: Delete occurrences of an element if it occurs more than n times
+Language: python
+Completed At: 2025-07-15 22:13:02 UTC
+https://www.codewars.com/kata/delete-occurrences-of-an-element-if-it-occurs-more-than-n-times
