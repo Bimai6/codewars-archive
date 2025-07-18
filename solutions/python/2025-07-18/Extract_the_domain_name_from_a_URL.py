a1c5aad3db34501d9d0788059557758854386840
Bimai6 <bimaivfx@gmail.com>
2025-07-18 14:21:38 +0000

    Auto: New solutions from Codewars

---
+++ solutions/python/2025-07-18/Extract_the_domain_name_from_a_URL.py
@@ -0,0 +1,4 @@
+Kata: Extract the domain name from a URL
+Language: python
+Completed At: 2025-07-18 12:12:37 UTC
+https://www.codewars.com/kata/extract-the-domain-name-from-a-url
---
+++ solutions/python/2025-07-18/Split_Strings.py
@@ -0,0 +1,4 @@
+Kata: Split Strings
+Language: python
+Completed At: 2025-07-18 14:16:49 UTC
+https://www.codewars.com/kata/split-strings
