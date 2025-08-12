10806356619cb158698e5ab793a315836e8aa779
Bimai6 <bimaivfx@gmail.com>
2025-08-12 14:21:41 +0000

    Auto: New solutions from Codewars

---
+++ solutions/python/2025-08-12/Complementary_DNA.py
@@ -0,0 +1,4 @@
+Kata: Complementary DNA
+Language: python
+Completed At: 2025-08-12 12:39:08 UTC
+https://www.codewars.com/kata/complementary-dna
---
+++ solutions/python/2025-08-12/The_reject___function.py
@@ -0,0 +1,4 @@
+Kata: The reject() function
+Language: python
+Completed At: 2025-08-12 13:03:02 UTC
+https://www.codewars.com/kata/the-reject-function
