fc65a4e89dcca2b695220dec0960b6be765e8920
Bimai6 <bimaivfx@gmail.com>
2025-09-08 14:20:49 +0000

    Auto: New solutions from Codewars

---
+++ solutions/javascript/2025-09-08/Prime_reversion.js
@@ -0,0 +1,4 @@
+Kata: Prime reversion
+Language: javascript
+Completed At: 2025-09-08 11:14:43 UTC
+https://www.codewars.com/kata/prime-reversion
---
+++ solutions/javascript/2025-09-08/Replace_With_Alphabet_Position.js
@@ -0,0 +1,4 @@
+Kata: Replace With Alphabet Position
+Language: javascript
+Completed At: 2025-09-08 09:02:16 UTC
+https://www.codewars.com/kata/replace-with-alphabet-position
