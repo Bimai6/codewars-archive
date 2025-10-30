3d1e136f947336b9d2fd5cf7dad65cfc64e1bdf8
Bimai6 <bimaivfx@gmail.com>
2025-10-30 14:21:24 +0000

    Auto: New solutions from Codewars

---
+++ solutions/javascript/2025-10-29/Count_characters_in_your_string.js
@@ -0,0 +1,4 @@
+Kata: Count characters in your string
+Language: javascript
+Completed At: 2025-10-29 17:08:06 UTC
+https://www.codewars.com/kata/count-characters-in-your-string
---
+++ solutions/javascript/2025-10-29/Your_order___please.js
@@ -0,0 +1,4 @@
+Kata: Your order,  please
+Language: javascript
+Completed At: 2025-10-29 14:47:57 UTC
+https://www.codewars.com/kata/your-order--please
