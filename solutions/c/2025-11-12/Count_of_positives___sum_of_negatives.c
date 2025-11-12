5a1d63e53e7382ee6e590e21bde50f50bf433b9c
Bimai6 <bimaivfx@gmail.com>
2025-11-12 14:22:23 +0000

    Auto: New solutions from Codewars

---
+++ solutions/c/2025-11-12/Count_of_positives___sum_of_negatives.c
@@ -0,0 +1,4 @@
+Kata: Count of positives / sum of negatives
+Language: c
+Completed At: 2025-11-12 12:20:01 UTC
+https://www.codewars.com/kata/count-of-positives--sum-of-negatives
---
+++ solutions/javascript/2025-11-12/Equal_Sides_Of_An_Array.js
@@ -0,0 +1,4 @@
+Kata: Equal Sides Of An Array
+Language: javascript
+Completed At: 2025-11-12 11:48:56 UTC
+https://www.codewars.com/kata/equal-sides-of-an-array
