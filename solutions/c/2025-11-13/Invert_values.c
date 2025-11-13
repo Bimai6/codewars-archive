ef95b1ee0bf31d4b6e5fc51c7015079b881ff9ed
Bimai6 <bimaivfx@gmail.com>
2025-11-13 14:20:13 +0000

    Auto: New solutions from Codewars

---
+++ solutions/c/2025-11-13/Invert_values.c
@@ -0,0 +1,4 @@
+Kata: Invert values
+Language: c
+Completed At: 2025-11-13 10:51:51 UTC
+https://www.codewars.com/kata/invert-values
---
+++ solutions/javascript/2025-11-13/Take_a_Number_And_Sum__Its_Digits_Raised_To_The_Consecutive_Powers_And______Eureka__.js
@@ -0,0 +1,4 @@
+Kata: Take a Number And Sum  Its Digits Raised To The Consecutive Powers And ....¡Eureka!!
+Language: javascript
+Completed At: 2025-11-13 10:45:08 UTC
+https://www.codewars.com/kata/take-a-number-and-sum--its-digits-raised-to-the-consecutive-powers-and-eureka
