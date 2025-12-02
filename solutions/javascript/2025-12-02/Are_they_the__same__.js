e1a1971af12610c669201f6ea4d0eb3351c14993
Bimai6 <bimaivfx@gmail.com>
2025-12-02 14:24:30 +0000

    Auto: New solutions from Codewars

---
+++ solutions/javascript/2025-12-02/Are_they_the__same__.js
@@ -0,0 +1,4 @@
+Kata: Are they the "same"?
+Language: javascript
+Completed At: 2025-12-02 12:48:36 UTC
+https://www.codewars.com/kata/are-they-the-same
---
+++ solutions/javascript/2025-12-02/Numerical_Palindrome__1_5.js
@@ -0,0 +1,4 @@
+Kata: Numerical Palindrome #1.5
+Language: javascript
+Completed At: 2025-12-02 13:34:26 UTC
+https://www.codewars.com/kata/numerical-palindrome-15
